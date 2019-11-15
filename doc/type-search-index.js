23fac4927f5b2c977b917b7c1047583b71b8c5e5
Victoire <56120737+VictoireMichel@users.noreply.github.com>
2019-11-15 09:32:33 +0100

    Création de la classe Adresse

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"model","l":"Adresse"},{"l":"All Classes","url":"allclasses-index.html"}]
---
+++ src/model/AdresseTest.java
@@ -0,0 +1,21 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AdresseTest {
+
+	@Test
+	void testModifierAdresse() {
+		Adresse a = new Adresse(1, 5, 1348, "avenue", "lln");
+		a.modifierAdresse(22, 1357, "rue du Brasseur", "Hélécine");
+		assertEquals(22, a.getNumero());
+		assertEquals(1357, a.getCodePostal());
+		assertEquals("rue du Brasseur", a.getRue());
+		assertEquals("Hélécine", a.getVille());
+	}
+	
+	//on ne peut pas tester l'exception car elle n'est pas encore implémentée
+
+}
