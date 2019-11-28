492a9ae34d8af6de7864ae45c8bd06d56c6b4ed2
Victoire <56120737+VictoireMichel@users.noreply.github.com>
2019-11-28 13:24:15 +0100

    avancement MVC (provisoire)

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -3,10 +3,26 @@
  */
 package controller;
 
+
+
+import model.Test;
+import view.Vue;
+
 /**
  * @author Victoire
  *
  */
 public class Controller {
+	Test model; // même problème que dans la vue
+	Vue vue;
 	
+	public Controller(Test model) {
+		this.model = model;
+	}
+	
+	//ajouter les méthodes
+	
+	public void addView(Vue vue) {
+		this.vue = vue;
+	}
 }
