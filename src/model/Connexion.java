b1ed6bb8c449e2b374406ffd796cbddb0b3bef2e
louisBrochet <louisbrochet@outlook.com>
2019-11-28 13:18:00 +0100

    classe connexion

---
+++ src/model/Connexion.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package model;
+
+import java.sql.*;
+/**
+ * @author lb
+ *
+ */
+public class Connexion {
+
+	private static String url= "jdbc:mysql://localhost/1formatikBdd";
+	private static String user = "root";
+	private static String mdp = "user1234";
+
+	private String req;
+	private Connection connexion;
+	private Statement requeteStatement;
+	private ResultSet resultat = null;
+	/**
+	 * @throws SQLException 
+	 * 
+	 */
+	public Connexion(String requete) throws SQLException {
+		req = requete;
+		connexion = DriverManager.getConnection(url, user, mdp); 
+		Statement requeteStatement = connexion.createStatement();
+		resultat = requeteStatement.executeQuery(req);
+	}
+
+	public void fermerConnexion() throws SQLException {
+		resultat.close();
+		requeteStatement.close();
+		connexion.close();
+	}
+}
